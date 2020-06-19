85d34522d14aa5b6a1f2418e53ee6c29fcc3ce8d
Marta Freitas <marta.freitas@mercadolivre.com>
2020-06-19 10:25:28 -0300

    ending the challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,13 @@ interface Balance {
   total: number;
 }
 
+
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +21,44 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce((
+      accumulator : Balance, 
+      transaction : Transaction) => {
+
+      switch (transaction.type){
+        case "income": 
+          accumulator.income += transaction.value;
+          break;
+        case "outcome": 
+          accumulator.outcome += transaction.value;
+          break;
+        default:
+          break;
+      }
+
+      return accumulator;
+
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+    )
+
+    return {...balance,
+            total: balance.income - balance.outcome
+          };
+
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type} : CreateTransaction): Transaction {
+    const transaction = new Transaction({title, value, type});
+    this.transactions.push(transaction);
+    return transaction; 
   }
 }
 
